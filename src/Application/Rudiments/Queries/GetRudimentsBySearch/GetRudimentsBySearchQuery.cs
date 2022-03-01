139fcf700660e844cd8755e1c6b4b2a3354d6e19
Kursat Toklucu <77143477+skursatToklucu@users.noreply.github.com>
2022-03-01 18:11:41 +0300

    search filter will be done

---
+++ src/Application/Rudiments/Queries/GetRudimentsBySearch/GetRudimentsBySearchQuery.cs
@@ -0,0 +1,11 @@
+﻿using DrumSpace.Application.Common.Models.Response;
+using DrumSpace.Application.Rudiments.Queries.Dtos;
+using MediatR;
+
+namespace DrumSpace.Application.Rudiments.Queries.GetRudimentsBySearch
+{
+    public class GetRudimentsBySearchQuery : IRequest<ListResponse<RudimentDto>>
+    {
+        public string Pattern { get; set; }
+    }
+}
---
+++ src/Application/Rudiments/Queries/GetRudimentsBySearch/GetRudimentsBySearchQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using DrumSpace.Application.Common.Interfaces;
+using DrumSpace.Application.Common.Models.Response;
+using DrumSpace.Application.Rudiments.Queries.Dtos;
+using MediatR;
+
+namespace DrumSpace.Application.Rudiments.Queries.GetRudimentsBySearch
+{
+    public class GetRudimentsBySearchQueryHandler : IRequestHandler<GetRudimentsBySearchQuery, ListResponse<RudimentDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+        public GetRudimentsBySearchQueryHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public Task<ListResponse<RudimentDto>> Handle(GetRudimentsBySearchQuery request, CancellationToken cancellationToken)
+        {
+            //TODO: to be continued
+
+            throw new NotImplementedException();
+        }
+    }
+}
