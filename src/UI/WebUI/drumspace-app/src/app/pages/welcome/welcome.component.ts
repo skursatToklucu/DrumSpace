dd0601cb1f7f14b084be638eee7182951ccf432b
Kursat Toklucu <77143477+skursatToklucu@users.noreply.github.com>
2022-04-12 10:55:40 +0300

    fabric js integration

--- src/UI/WebUI/drumspace-app/src/app/pages/welcome/welcome.component.ts
+++ src/UI/WebUI/drumspace-app/src/app/pages/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import Vex from 'vexflow';
+import {Component, ViewChild, OnInit} from '@angular/core';
+// @ts-ignore
+import {FabricjsEditorComponent} from 'helpers/projects/angular-editor-fabric-js/src/public-api';
 
 
 @Component({
@@ -8,36 +9,145 @@ import Vex from 'vexflow';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-  VF: Vex.Flow;
-  score;
-  system;
+  title = 'angular-editor-fabric-js';
+
+  @ViewChild('canvas', {static: false}) canvas: FabricjsEditorComponent;
+
+  public rasterize() {
+    this.canvas.rasterize();
+  }
+
+  public rasterizeSVG() {
+    this.canvas.rasterizeSVG();
+  }
+
+  public saveCanvasToJSON() {
+    this.canvas.saveCanvasToJSON();
+  }
+
+  public loadCanvasFromJSON() {
+    this.canvas.loadCanvasFromJSON();
+  }
+
+  public confirmClear() {
+    this.canvas.confirmClear();
+  }
+
+  public changeSize() {
+    this.canvas.changeSize();
+  }
+
+  public addText() {
+    this.canvas.addText();
+  }
+
+  public getImgPolaroid(event) {
+    this.canvas.getImgPolaroid(event);
+  }
+
+  public addImageOnCanvas(url) {
+    this.canvas.addImageOnCanvas(url);
+  }
+
+  public readUrl(event) {
+    this.canvas.readUrl(event);
+  }
+
+  public removeWhite(url) {
+    this.canvas.removeWhite(url);
+  }
+
+  public addFigure(figure) {
+    this.canvas.addFigure(figure);
+  }
+
+  public removeSelected() {
+    this.canvas.removeSelected();
+  }
+
+  public sendToBack() {
+    this.canvas.sendToBack();
+  }
+
+  public bringToFront() {
+    this.canvas.bringToFront();
+  }
+
+  public clone() {
+    this.canvas.clone();
+  }
+
+  public cleanSelect() {
+    this.canvas.cleanSelect();
+  }
+
+  public setCanvasFill() {
+    this.canvas.setCanvasFill();
+  }
+
+  public setCanvasImage() {
+    this.canvas.setCanvasImage();
+  }
+
+  public setId() {
+    this.canvas.setId();
+  }
+
+  public setOpacity() {
+    this.canvas.setOpacity();
+  }
+
+  public setFill() {
+    this.canvas.setFill();
+  }
+
+  public setFontFamily() {
+    this.canvas.setFontFamily();
+  }
+
+  public setTextAlign(value) {
+    this.canvas.setTextAlign(value);
+  }
+
+  public setBold() {
+    this.canvas.setBold();
+  }
+
+  public setFontStyle() {
+    this.canvas.setFontStyle();
+  }
+
+  public hasTextDecoration(value) {
+    this.canvas.hasTextDecoration(value);
+  }
+
+  public setTextDecoration(value) {
+    this.canvas.setTextDecoration(value);
+  }
+
+  public setFontSize() {
+    this.canvas.setFontSize();
+  }
+
+  public setLineHeight() {
+    this.canvas.setLineHeight();
+  }
+
+  public setCharSpacing() {
+    this.canvas.setCharSpacing();
+  }
+
+  public rasterizeJSON() {
+    this.canvas.rasterizeJSON();
+  }
+
+  public drawMode() {
+    this.canvas.drawingMode();
+  }
 
   constructor() {
   }
 
   ngOnInit() {
-    const {Factory, EasyScore, System} = Vex.Flow;
-
-    const vf = new Factory({
-      renderer: {elementId: 'output', width: 500, height: 200},
-    });
-
-    const score = vf.EasyScore();
-    const system = vf.System();
-
-    system
-      .addStave({
-        voices: [
-          score.voice(score.notes('C#5/q, B4, A4, G#4', {stem: 'up'})),
-          // score.voice(score.notes('C#4/h, C#4', {stem: 'down'})),
-        ],
-      })
-      .addClef('treble')
-      .addTimeSignature('4/4');
-
-    vf.draw();
-  }
-
-  createStave() {
   }
 }
--- src/UI/WebUI/drumspace-app/src/app/pages/welcome/welcome.module.ts
+++ src/UI/WebUI/drumspace-app/src/app/pages/welcome/welcome.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-
-import { WelcomeRoutingModule } from './welcome-routing.module';
-
-import { WelcomeComponent } from './welcome.component';
+import {NgModule} from '@angular/core';
+import {WelcomeRoutingModule} from './welcome-routing.module';
+import {WelcomeComponent} from './welcome.component';
 import {NzGridModule} from "ng-zorro-antd/grid";
+import {FormsModule} from '@angular/forms';
+import {FabricjsEditorModule} from "../../_helpers/projects/lib/angular-editor-fabric-js.module";
 
 
 @NgModule({
-    imports: [WelcomeRoutingModule, NzGridModule],
+  imports: [WelcomeRoutingModule, NzGridModule, FabricjsEditorModule, FormsModule],
   declarations: [WelcomeComponent],
-  exports: [WelcomeComponent]
+  exports: [WelcomeComponent],
+  bootstrap: [WelcomeComponent]
 })
-export class WelcomeModule { }
+export class WelcomeModule {
+}
